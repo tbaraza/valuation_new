2a13a46cf8d478b07845c44533a0be64969a38c6
Tonida <tonidahnjeli@gmail.com>
2016-02-29 18:41:59 +0300

    database

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 import os
+import psycopg2
+import urlparse
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 
 class Config:
